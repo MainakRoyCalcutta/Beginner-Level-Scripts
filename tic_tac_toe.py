c5e4fad8236ad55258c1c017def6f425731bee6c
Mainak Roy <75011220+MainakRoyCalcutta@users.noreply.github.com>
2021-02-21 22:55:51 +0530

    Comments Added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,8 @@
+"""
+TIC TAC TOE GAME
+Basic Using PlaceHolder Numbers: Top left corner as zero and bottom right corner 8
+"""
+#Board function
 board = [" "] * 9
 def my_board(board):
     print(" " + board[0] + "  | " + board[1] + " |  " + board[2] + " ")
@@ -6,6 +11,7 @@ def my_board(board):
     print(" --- --- --- ")
     print(" " + board[6] + "  | " + board[7] + " |  " + board[8] + " ")
 
+#Accepting player choice for first player, the second player automatically get the the other symbol    
 def player_choice():
     play = False
     print("Welcome to the Game\n")
@@ -23,27 +29,34 @@ def player_choice():
             play = True
             print(f"PLAYER 1: {pl1} \nPLAYER 2: {pl2}")
         else:
+            #Wrong Input's Output
             print("Wrong Input!!! Please Choose X or O")
             play = False
     return pl1, pl2
 
+#Function to accept Player's choice input i.e. where the user wants to place its symbol and return the input position
 def player_inpt():
+    #Input is not taken as integer as if there was an non-number input, .isdigit condition canot be applied
     indx_inpt = input("Enter where you want to place (0 - 8) : ")
     while indx_inpt.isdigit() :
         if int(indx_inpt) < 9 and int(indx_inpt) > -1:
             if board[int(indx_inpt)] != "X" and board[int(indx_inpt)] != "O":
                 break
             else:
+                #If the place is already filled with any symbol then output
                 print("You cannot overwrite!!")
                 return player_inpt()
         else:
+            #Invalid input's output if input range not between 0 or 8
             print("Invalid Input!! Enter an index between 0 to 8")
             return player_inpt()
     else:
+        #Invalid input's output if input not a number
         print("Invalid Input!! Enter an index between 0 to 8")
         return player_inpt()
     return int(indx_inpt)
 
+#Winning Condition
 def winner(playr):
     a = [playr, playr, playr]
     if [board[0], board[1], board[2]] == a or [board[3], board[4], board[5]] == a or [board[6], board[7],
@@ -57,6 +70,7 @@ def winner(playr):
     else:
         pass
 
+#Function for asking if user wants to play again
 def play_agn():
     pl_agn = input("Do you want to play again ? (Y/N) : ")
     pl_agn = pl_agn.upper()
@@ -70,6 +84,7 @@ def play_agn():
         if pl_agn == "N":
             print("Thank you")
 
+#Final Function to replace the empty places by X or O
 def replce(player_choice):
     pl1, pl2 =player_choice
     while set(board) != {"X", "O"}:
@@ -91,11 +106,12 @@ def replce(player_choice):
     else:
         print("Match Draw")
         return play_agn()
-
+#reset the used board
 def reset_board():
     for _ in range(0,9):
         board[_] = " "
     my_board(board)
 
+#Final Fuction calls    
 my_board(board)
 replce(player_choice())
